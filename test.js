89c57581227b171efcde2bafe41c8ef55f44c53c
ZECTBynmo <michael.vegeto@gmail.com>
2013-01-25 02:16:15 -0500

    test script

---
+++ test.js
@@ -0,0 +1,1 @@
+// This test script inserts "TEST" in the beginning of every line of README.mdvar changer = require("./changer").createChanger();// Create a rulevar rule = function( lineNumber, text ) {	if( true ) {		text = "TEST " + text;	}			return text;}var fileName = "README.md";changer.change( rule, fileName, function() {	console.log( "Changed some stuff!" );});
