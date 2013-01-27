8fe681b950bbd524f7c94d5a0e26dec1f44c469a
ZECTBynmo <michael.vegeto@gmail.com>
2013-01-27 16:38:49 -0500

    update the readme to include Changer.insert() and fix line endings

--- test.js
+++ test.js
@@ -1,1 +1,21 @@
-// This test script inserts "TEST" in the beginning of every line of README.mdvar changer = require("./changer").createChanger();// Create a rulevar rule = function( lineNumber, text ) {	if( true ) {		text = "TEST " + text;	}			return text;}var fileName = "README.md";changer.change( rule, fileName, function() {	console.log( "Changed some stuff!" );});
+// This test script inserts "TEST" in the beginning of every line of README.md
+var changer = require("./changer").createChanger();
+
+// Create a rule
+var rule = function( lineNumber, text ) {
+	if( true ) {
+		text = "TEST " + text;
+	}		
+
+	return text;
+}
+
+var fileName = "README.md";
+
+changer.change( rule, fileName, function() {
+	console.log( "Changed some stuff!" );
+
+	changer.insert( "/////////////////////////////////////\n////////////////////////////\n///////////////////////////", fileName, 5, function(){
+		console.log("Inserted some stuff")
+	});
+});
